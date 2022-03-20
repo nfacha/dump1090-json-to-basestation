786a1b610374bf5558d2cc4ed119f3c046aae3fb
Nuno Facha <nunofacha@gmail.com>
2022-03-20 21:45:15 -0100

    WIP

---
+++ src/basestation/BaseStationMessage.ts
@@ -0,0 +1,49 @@
+export class BaseStationMessage{
+    public messageType = 'MSG';
+    public onGround = false;
+    public sessionId = 0;
+    public aircraftId = 0;
+    public hexIdent = '';
+    public flightId = 0;
+    public dateMessageGenerated = '';
+    public timeMessageGenerated = '';
+    public dateMessageLogged = '';
+    public timeMessageLogged = '';
+    public callsign = '';
+    public groundSpeed = 0;
+    public track = 0;
+    public latitude = 0;
+    public longitude = 0;
+    public verticalrate = 0;
+    public squawk = 0;
+    public squawkHasChanged = 0;
+    public emergency = 0;
+    public identActive = 0;
+    public altitude = 0;
+
+
+    constructor(hexIdent: string, callsign: string, groundSpeed: number, track: number, latitude: number, longitude: number, verticalrate: number, squawk: number, altitude: number){
+        let currentDate = new Date()
+        let day = currentDate.getDate()
+        let month = currentDate.getMonth() + 1
+        let year = currentDate.getFullYear()
+        this.hexIdent = hexIdent;
+        this.dateMessageGenerated = year + '/' + month + '/' + day;
+        this.timeMessageGenerated = currentDate.toTimeString().split(' ')[0];
+        this.dateMessageLogged = this.dateMessageGenerated;
+        this.timeMessageLogged = this.timeMessageGenerated;
+        this.callsign = callsign;
+        this.groundSpeed = groundSpeed;
+        this.track = track;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.verticalrate = verticalrate;
+        this.squawk = squawk;
+        this.altitude = altitude;
+    }
+
+    public generate(){
+        const transmissionMessageType = this.onGround ? '2':'3';
+        return `${this.messageType},${transmissionMessageType},${this.sessionId},${this.aircraftId},${this.hexIdent},${this.flightId},${this.dateMessageGenerated},${this.timeMessageGenerated},${this.dateMessageLogged},${this.timeMessageLogged},${this.callsign},${this.altitude},${this.groundSpeed},${this.track},${this.latitude},${this.longitude},${this.verticalrate},${this.squawk},${this.squawkHasChanged},${this.emergency},${this.identActive},${this.onGround ? -1 : 0}\n`;
+    }
+}
--- src/encoder.ts
+++ src/encoder.ts
@@ -1,5 +1,7 @@
 import {Logger} from "tslog";
 import * as fs from "fs";
+import {BaseStationMessage} from "./basestation/BaseStationMessage";
+import axios from "axios";
 
 class Encoder {
     public log: Logger;
@@ -10,9 +12,54 @@ class Encoder {
         this.log = new Logger();
         this.log.info("Encoder started");
         this.config = this.loadConfig();
+        this.log.info("Config loaded");
+        setInterval(() => {
+            this.updateData();
+        }, this.config['refreshInterval'] * 1000);
+        this.updateData();
     }
 
     private loadConfig() {
         return JSON.parse(fs.readFileSync("./config.json", "utf8"));
     }
+
+    public parsePlaneList(data: any, format: string): string {
+        let rx = '';
+        if(format === 'data-json'){
+            for (const aircraft of data) {
+                if(aircraft.validposition !== 1){
+                    this.log.debug("Invalid position");
+                    continue;
+                }
+                this.log.debug(aircraft.hex);
+                rx += new BaseStationMessage(aircraft.hex, aircraft.flight.trim(),aircraft.speed, aircraft.track,aircraft.lat, aircraft.lon,aircraft.vert_rate, aircraft.squawk, aircraft.altitude).generate();
+            }
+        }else if(format === 'aircraft-json'){
+            for (const aircraft of data.aircraft) {
+                rx += new BaseStationMessage(aircraft.hex, aircraft.flight.trim(), aircraft.gs, aircraft.track, aircraft.lat, aircraft.lon, aircraft.baro_rate, aircraft.squawk, aircraft.alt_baro).generate();
+            }
+        }
+
+        return rx
+    }
+
+    public async updateData() {
+        let rx = '';
+        for (const server of this.config['servers']) {
+            try {
+                const serverData = await axios.get(server['host']);
+                rx += this.parsePlaneList(serverData.data, server['format']);
+            }catch (e) {
+                this.log.error(e);
+            }
+        }
+
+        //broadcast to socket
+        this.log.info("Sending data to socket");
+        this.log.info(rx);
+    }
+
 }
+
+export const Dump1090Encoder = new Encoder();
+
