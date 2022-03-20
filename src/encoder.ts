2fb7c4b820aa8056b3365d5060aa79db20e42849
Nuno Facha <nunofacha@gmail.com>
2022-03-20 22:05:58 -0100

    WIP

--- src/encoder.ts
+++ src/encoder.ts
@@ -2,10 +2,13 @@ import {Logger} from "tslog";
 import * as fs from "fs";
 import {BaseStationMessage} from "./basestation/BaseStationMessage";
 import axios from "axios";
+import {Client, TCPServer} from "pocket-sockets";
+
 
 class Encoder {
     public log: Logger;
     public config: any;
+    public socketServer: TCPServer;
 
 
     constructor() {
@@ -13,6 +16,16 @@ class Encoder {
         this.log.info("Encoder started");
         this.config = this.loadConfig();
         this.log.info("Config loaded");
+        this.socketServer = new TCPServer({
+            host: "127.0.0.1",
+            port: 20004,
+        });
+        this.socketServer.listen();
+        this.socketServer.onConnection((client: Client) => {
+            this.log.info("Client connected: " + client.clientOptions?.host + ":" + client.clientOptions?.port);
+        });
+
+
         setInterval(() => {
             this.updateData();
         }, this.config['refreshInterval'] * 1000);
@@ -31,7 +44,6 @@ class Encoder {
                     this.log.debug("Invalid position");
                     continue;
                 }
-                this.log.debug(aircraft.hex);
                 rx += new BaseStationMessage(aircraft.hex, aircraft.flight.trim(),aircraft.speed, aircraft.track,aircraft.lat, aircraft.lon,aircraft.vert_rate, aircraft.squawk, aircraft.altitude).generate();
             }
         }else if(format === 'aircraft-json'){
@@ -49,14 +61,17 @@ class Encoder {
             try {
                 const serverData = await axios.get(server['host']);
                 rx += this.parsePlaneList(serverData.data, server['format']);
-            }catch (e) {
+            } catch (e) {
                 this.log.error(e);
             }
         }
 
         //broadcast to socket
         this.log.info("Sending data to socket");
-        this.log.info(rx);
+        // this.log.info(rx);
+        this.socketServer.clients.forEach((client: Client) => {
+            client.sendString(rx);
+        });
     }
 
 }
